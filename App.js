3263a5dcc29497b44e3bf9c0c700674cb452fdbb
Dmitry Yurov <yurov37@gmail.com>
2023-02-02 12:02:52 +0200

    hw #2 almost done

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import RegistrationScreen from "./components/Screens/RegistrationScreen.jsx";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App. Student Yurov.</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.image}
+          source={require("./assets/images/photo-bg.png")}
+        >
+          <RegistrationScreen />
+        </ImageBackground>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
   },
 });
